f29d28341f17b259d4173b962b5ecea4f5a77d64
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-07-09 07:20:51 +0300

    update

--- node-farm/index.js
+++ node-farm/index.js
@@ -7,3 +7,8 @@ console.log(textIn);
 const textOut = `this is what we know about avocado: ${textIn}\nCreated on${Date.now()}`;
 fs.writeFileSync("./txt/output.txt", textOut);
 console.log("done");
+
+//Non Blocking
+fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+  console.log(data);
+});
