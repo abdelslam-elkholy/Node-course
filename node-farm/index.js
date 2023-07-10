1eccb8b66d8224c6ce15476a4b6f02209bfab5b7
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-07-10 05:59:25 +0300

    Uodate

--- node-farm/index.js
+++ node-farm/index.js
@@ -25,6 +25,20 @@ const fs = require("fs");
 /////////////////////////////
 //Server
 
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const products = JSON.parse(data);
 
@@ -32,7 +46,8 @@ const server = http.createServer((req, res) => {
   const pathName = req.url;
   if (pathName === "/" || pathName === "/overview") {
     console.log(pathName);
-    res.end("This is overView");
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(tempOverview);
   } else if (pathName === "/product") {
     console.log(pathName);
     res.end("This is product");
