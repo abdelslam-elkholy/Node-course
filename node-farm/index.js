555500db0c66d216d6368055d3271165bef427b5
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-07-11 05:01:35 +0300

    Update

--- node-farm/index.js
+++ node-farm/index.js
@@ -25,10 +25,24 @@ const fs = require("fs");
 /////////////////////////////
 //Server
 
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
+
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   "utf-8"
 );
+
 const tempCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
   "utf-8"
@@ -47,6 +61,9 @@ const server = http.createServer((req, res) => {
   if (pathName === "/" || pathName === "/overview") {
     console.log(pathName);
     res.writeHead(200, { "Content-type": "text/html" });
+
+    const cardsHtml = products.map((el) => replaceTemplate(tempCard, el));
+    console.log(cardsHtml);
     res.end(tempOverview);
   } else if (pathName === "/product") {
     console.log(pathName);
