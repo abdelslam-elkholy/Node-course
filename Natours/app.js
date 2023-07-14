ef27e06b1d887a4838fa402a5c9916f7ff3c6fa9
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-07-14 13:51:43 +0300

    update

--- Natours/app.js
+++ Natours/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 const tourRouter = require("./routs/toursRouts");
 const userRouter = require("./routs/usersRouts");
-app.use(express.json());
 
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
--- Natours/server.js
+++ Natours/server.js
@@ -1,6 +1,8 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
-const port = 3000;
+const port = dotenv.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening from porst ${port}`);
 });
