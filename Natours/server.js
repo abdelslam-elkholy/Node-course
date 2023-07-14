18f4c535a14302aa4f8f9a02501de65ad302bbda
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-07-14 18:10:43 +0300

    update

--- Natours/server.js
+++ Natours/server.js
@@ -1,6 +1,18 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+dbConnect().catch((err) => console.log(err));
+
+async function dbConnect() {
+  await mongoose.connect(DB);
+}
 
 const port = dotenv.PORT || 3000;
 app.listen(port, () => {
