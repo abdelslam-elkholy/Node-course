b730a6102a0f622d979521b02e5ecbb586a11df3
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-07-23 21:50:51 +0300

    update

--- Natours/app.js
+++ Natours/app.js
@@ -13,6 +13,17 @@ app.all("*", (req, res, next) => {
   const err = new Error(`Can't find ${req.url} in the server`);
   err.status = "fail";
   err.statusCode = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    staus: err.status,
+    message: err.message,
+  });
 });
 
 module.exports = app;
