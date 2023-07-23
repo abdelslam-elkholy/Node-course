7a40f7d858897465d5b4251f1441e3b01ce516d7
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-07-23 20:14:36 +0300

    Update

--- Natours/app.js
+++ Natours/app.js
@@ -9,4 +9,10 @@ const userRouter = require("./routs/usersRouts");
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
+app.all("*", (req, res, next) => {
+  const err = new Error(`Can't find ${req.url} in the server`);
+  err.status = "fail";
+  err.statusCode = 404;
+});
+
 module.exports = app;
