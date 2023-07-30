a4c4d413d63bbb23cba67d62f80d40b3c8757d81
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-07-30 15:34:04 +0300

    update

---
+++ Natours/controllers/authController.js
@@ -0,0 +1,12 @@
+const User = require("./../models/userModel");
+const catchAsync = require("./../utils/catchAsync");
+exports.signUp = catchAsync(async (req, res, next) => {
+  const user = User.create(req.body);
+
+  res.status(201).json({
+    message: "Done",
+    data: {
+      user,
+    },
+  });
+});
